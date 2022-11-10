5e351776ac27de940bd669db69acc2ea1fc07087
raifreire <rai.freire26@gmail.com>
2022-11-09 22:05:04 -0300

    add Component Header

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { Header } from './components/Header/Header';
+import { Content } from './components/Content/Content';
+
+import './global.css';
+
+export function App() {
+    return (
+        <div>
+            <Header></Header>
+            <Content></Content>
+        </div>
+    )
+}
